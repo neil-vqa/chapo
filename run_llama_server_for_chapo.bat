8004d93795d2f791b832dc967a1e5539a144b870
neil-vqa <nvq.alino@gmail.com>
2024-09-16 22:39:57 +0800

    min window

--- run_llama_server_for_chapo.bat
+++ run_llama_server_for_chapo.bat
@@ -6,4 +6,4 @@ set LlamaServerPath=%~dp0llama-server.exe
 set ModelPath=%~dp0models\gemma-2-2b-it-abliterated-Q4_K_M.gguf
 
 :: Run
-start "Chapo x Llama Server (Do not close this window when Chapo is running)" cmd /c %LlamaServerPath% -m %ModelPath%
+start "Chapo x Llama Server (Do not close this window when Chapo is running)" /min %LlamaServerPath% -m %ModelPath%
