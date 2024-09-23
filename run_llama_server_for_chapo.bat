e7b25cfa7abcf3be0314167207ab096658111cea
neil-vqa <nvq.alino@gmail.com>
2024-09-24 07:17:20 +0800

    remove variables

--- run_llama_server_for_chapo.bat
+++ run_llama_server_for_chapo.bat
@@ -1,9 +1,3 @@
 @echo off
-setlocal
-
-:: Set the path to the llama-server.exe file and the model file
-set LlamaServerPath=%~dp0llama-server.exe
-set ModelPath=%~dp0models\gemma-2-2b-it-abliterated-Q4_K_M.gguf
-
 :: Run
-start "Chapo x Llama Server (Do not close this window when Chapo is running)" /min %LlamaServerPath% -m %ModelPath%
+start "Chapo x Llama Server (Do not close this window when Chapo is running)" /min llama-server.exe -m models\gemma-2-2b-it-abliterated-Q4_K_M.gguf
